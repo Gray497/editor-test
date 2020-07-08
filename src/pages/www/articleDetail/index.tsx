832a3f27243a13b5e9c31f1652ca5ca9e35d3275
497 <guoweipeng@weya-tech.com>
2020-07-08 19:03:18 +0800

    1

--- src/pages/www/articleDetail/index.tsx
+++ src/pages/www/articleDetail/index.tsx
@@ -1,12 +1,24 @@
+import React from 'react';
+import styles from './index.less';
+import { connect, Link } from 'umi';
 
-import React from 'react'
-import styles from './index.less'
+const PATH = 'www/articleDetail';
 
-export default (props) => {
-  return <div className={styles.wrap}>
-    123
-    {/*{[*/}
-    {/*  {route: '/gmxl'}*/}
-    {/*]}*/}
-  </div>
+// @ts-ignore
+@connect(({ [PATH]: _model }, dispatch) => ({
+  dispatch,
+  _model,
+}))
+export default class Index extends React.Component {
+
+  render() {
+    const { _model:{detail}, location } = this.props;
+    console.log(this.props);
+    return <div className={styles.wrap}>
+      {detail.content}
+      {/*{[*/}
+      {/*  {route: '/gmxl'}*/}
+      {/*]}*/}
+    </div>;
+  }
 }
